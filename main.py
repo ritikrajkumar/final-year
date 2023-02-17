478f7c34098dfea235726e17fc6207aa8dbb2ce3
ritikrajkumar <ritikrajkumar007@gamil.com>
2023-02-18 00:03:24 +0530

    imp

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pybase64
 import streamlit as st
 import os
 from PIL import Image
@@ -6,14 +7,15 @@ import pickle
 import tensorflow
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.layers import GlobalMaxPooling2D
-from tensorflow.keras.applications.resnet50 import ResNet50,preprocess_input
+from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
 from sklearn.neighbors import NearestNeighbors
 from numpy.linalg import norm
 
-feature_list = np.array(pickle.load(open('embeddings.pkl','rb')))
-filenames = pickle.load(open('filenames.pkl','rb'))
+feature_list = np.array(pickle.load(open('embeddings.pkl', 'rb')))
+filenames = pickle.load(open('filenames.pkl', 'rb'))
 
-model = ResNet50(weights='imagenet',include_top=False,input_shape=(224,224,3))
+model = ResNet50(weights='imagenet', include_top=False,
+                 input_shape=(224, 224, 3))
 model.trainable = False
 
 model = tensorflow.keras.Sequential([
@@ -23,15 +25,17 @@ model = tensorflow.keras.Sequential([
 
 st.title('Fashion Recommendation System')
 
+
 def save_uploaded_file(uploaded_file):
     try:
-        with open(os.path.join('uploads',uploaded_file.name),'wb') as f:
+        with open(os.path.join('uploads', uploaded_file.name), 'wb') as f:
             f.write(uploaded_file.getbuffer())
         return 1
     except:
         return 0
 
-def feature_extraction(img_path,model):
+
+def feature_extraction(img_path, model):
     img = image.load_img(img_path, target_size=(224, 224))
     img_array = image.img_to_array(img)
     expanded_img_array = np.expand_dims(img_array, axis=0)
@@ -41,13 +45,38 @@ def feature_extraction(img_path,model):
 
     return normalized_result
 
-def recommend(features,feature_list):
-    neighbors = NearestNeighbors(n_neighbors=6, algorithm='brute', metric='euclidean')
+
+def recommend(features, feature_list):
+    neighbors = NearestNeighbors(
+        n_neighbors=6, algorithm='brute', metric='euclidean')
     neighbors.fit(feature_list)
 
     distances, indices = neighbors.kneighbors([features])
 
     return indices
+
+
+def get_pybase64(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return pybase64.b64encode(data).decode()
+
+
+def set_background(png_file):
+    bin_str = get_pybase64(png_file)
+    page_bg_img = '''
+    <style>
+    .stApp {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
+
+set_background('background.png')
+
 
 # steps
 # file upload -> save
@@ -58,12 +87,13 @@ if uploaded_file is not None:
         display_image = Image.open(uploaded_file)
         st.image(display_image)
         # feature extract
-        features = feature_extraction(os.path.join("uploads",uploaded_file.name),model)
-        #st.text(features)
+        features = feature_extraction(os.path.join(
+            "uploads", uploaded_file.name), model)
+        # st.text(features)
         # recommendention
-        indices = recommend(features,feature_list)
+        indices = recommend(features, feature_list)
         # show
-        col1,col2,col3,col4,col5 = st.columns(5)
+        col1, col2, col3, col4, col5 = st.columns(5)
 
         with col1:
             st.image(filenames[indices[0][0]])
@@ -77,4 +107,3 @@ if uploaded_file is not None:
             st.image(filenames[indices[0][4]])
     else:
         st.header("Some error occured in file upload")
-
